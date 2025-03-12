2c4c874677d90b4fc697cba792a4bbd8b0f2fcd6
Natasha-n-j <natashanjkaranja@gmail.com>
2025-03-12 17:47:32 +0300

    added formState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import { useState } from "react";
 import FormComponent from "./Components/FormComponent";
 import LogInComponent from "./Components/LogInComponent";
 
 const App = () => {
-  const handleForm = () => {
-    console.log("Form submmited")
+  const [formState, setFormState] = useState("register")
+  const handleForm = (e) => {
+    const btnName = e.target.value;
+    setFormState(btnName)
+    console.log(formState)
   }
 
   return (
     <div className="App">
-      <button onClick={handleForm}>Register</button>
-      <button onClick={handleForm}>Login</button>
-      <FormComponent />
-      <LogInComponent />
+      <button onClick={handleForm} value="register">Register</button>
+      <button onClick={handleForm} value="login">Login</button>
+      {formState === "register" ? 
+        <FormComponent /> :
+        <LogInComponent />
+      }
     </div>
   );
 }
