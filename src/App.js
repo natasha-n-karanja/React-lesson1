1ab83acfb406a5f07cb1c6a705ac0d42fcade405
Natasha-n-j <natashanjkaranja@gmail.com>
2025-03-12 17:11:53 +0300

    added login from

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import FormComponent from "./Components/FormComponent";
+import LogInComponent from "./Components/LogInComponent";
 
 const App = () => {
   return (
     <div className="App">
       <FormComponent />
+      <LogInComponent />
     </div>
   );
 }
