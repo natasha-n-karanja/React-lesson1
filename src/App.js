1da202ff2f8947e449c80f1f977fc1cfb4c681a8
Natasha-n-j <natashanjkaranja@gmail.com>
2025-03-12 17:30:08 +0300

    added new function

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import FormComponent from "./Components/FormComponent";
 import LogInComponent from "./Components/LogInComponent";
 
 const App = () => {
+  const handleForm = () => {
+    console.log("Form submmited")
+  }
+
   return (
     <div className="App">
+      <button onClick={handleForm}>Register</button>
+      <button onClick={handleForm}>Login</button>
       <FormComponent />
       <LogInComponent />
     </div>
