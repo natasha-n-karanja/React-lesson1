3c3510834fe770a58e4feb22494d44741157a1fa
natashankaranja <natashankaranja@gmail.com>
2025-03-19 10:35:06 +0300

    added header component

---
+++ src/Components/HeaderComponent.js
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import "./HeaderComponent.css"
+import { Link } from "react-router-dom"
+
+
+const HeaderComponent = () => {
+    const [links] = useState([
+        "Home",
+        "About",
+        "Contact",
+        "Auth"
+    ])
+    return(
+        <header>
+            {links.map((li, key) =>{
+                return(
+                    <div key={key}>
+                        <Link className="nav-links" to={`/${li}`}>{li}</Link>
+                    </div>
+                )
+            })}
+        </header>
+    )
+}
+
+export default HeaderComponent
