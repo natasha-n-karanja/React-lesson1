40412c51ff4076bc0c7ed84d594ef06dd841f14b
natashankaranja <natashankaranja@gmail.com>
2025-03-19 17:32:22 +0300

    added new component

--- src/Components/ProfileComponent.js
+++ src/Components/ProfileComponent.js
@@ -1,26 +1,29 @@
 import { useState } from "react";
-import {link} from "react-router-dom";
 import React from 'react';
 import './ProfileComponent.css';
+import ProfileData from "../Data/Profile.json"
 
 
 const ProfileComponent = () => {
-    const [team] = useState ([
-        {
-            name: " Natasha N Karanja",
-            position: "  Deputy Lead Developer",
-            description: "I'm an ambitous and passionate developer."
-        }
-    ])
+  const [team,setTeam] = useState([])
+
+  React.useEffect(() => {
+    setTeam(ProfileData)
+  }, [])
 
     return (
        <div>
-            <h1>Meet The Team</h1>
-            <header>
-                
-            </header>
-            </div>
-    )
-}
+            <h1>Meet The Team</h1> 
+            {team?.team_members?.map((val, key) => {
 
-export default ProfileComponent;
+             return(
+              <div key ={key}>
+                <h1>{val.name}</h1>
+              </div>
+             )
+            })}
+        </div>
+    )
+
+  }
+export default ProfileComponent
